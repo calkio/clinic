82121c6eab7062b011a9a4bbd6ea7769b26bd893
Данил Суров <danil.surov.03@mail.ru>
2023-09-19 16:42:33 +0700

    fix

--- clinic/Entities/Doctor.cs
+++ clinic/Entities/Doctor.cs
@@ -8,12 +8,22 @@ namespace clinic.Entities
 {
     internal class Doctor
     {
+        public Doctor(int id, string firstname, string secondname, string surname, int experience, string qualification)
+        {
+            _id = id;
+            _firstname = firstname;
+            _secondname = secondname;
+            _surname = surname;
+            _experience = experience;
+            _qualification = qualification;
+        }
+
         int _id;
         string _firstname;
         string _secondname;
         string _surname;
         int _experience;
-        int _qualification;
+        string _qualification;
         Schedule _schedule;
         int _numberOfCabinet;
 
